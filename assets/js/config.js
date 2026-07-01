5074234eefcec2a54095f07c37e98f0fe0ff0e30
Shiloh <inininicole@gmail.com>
2026-07-01 21:04:11 +0200

    主页更新

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,10 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbx9jRY5_gZLCksvr79mvvD4BptYaQAD-FbTVJ_roS6kgss3YXj5VBfVOoLwZMZMF6sSaw/exec"
+
+const contacts = {
+    telegram: "https://t.me/+38162389638",
+    viber: "https://invite.viber.com/?g2=AQASZejnvCaReVay22AlJNkH965hZ67J2LVqYAL73SnJddt9aaEtZOX9dOJsNAfz",
+    whatsapp: "https://wa.me/message/E3PIYL2X2SPFN1",
+    instagram: "https://www.instagram.com/kaola_bubbletea?igsh=eWo4aW0yMDF4NHVq&utm_source=qr",
+    facebook: "https://www.facebook.com/share/1eYa3f3ZNT/?mibextid=wwXIfr",
+    tiktok: "https://www.tiktok.com/@kaolabubbletea?_r=1&_t=ZS-97fzIjCoIbw"
+}
