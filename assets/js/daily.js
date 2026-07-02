56390b026654f0542ef637cce1d0e0237a3e3d18
Shiloh <inininicole@gmail.com>
2026-07-02 20:03:42 +0200

    每日记录样式增加

--- assets/js/daily.js
+++ assets/js/daily.js
@@ -4,20 +4,23 @@ document.getElementById('dailyForm').onsubmit = async e => {
     form.append("submitType", "daily");
     form.append("votes", document.getElementById("votes").value);
     form.append("cash", document.getElementById("cash").value);
-    form.append("rmb", document.getElementById("rmb").value);
-    form.append("addconsumeress", document.getElementById("consume").value);
-    form.append("wolt", document.getElementById("wolt").value);
-    form.append("material", document.getElementById("material").value);
-    form.append("remark", document.getElementById("remark").value);
+    form.append("rmb", document.getElementById("rmb").value || 0);
+    form.append("addconsumeress", document.getElementById("consume").value || 0);
+    form.append("wolt", document.getElementById("wolt").value || 0);
+    form.append("material", document.getElementById("material").value || "");
+    form.append("remark", document.getElementById("remark").value || "");
 
     fetch(API_URL, {
         method: "POST",
         body: form
     })
-    .then(() => {
-      console.log("success")
-    })
+    alert("success")
+    location.href = "/";
+    // .then(() => {
+    //   alert("success")
+    // })
     // .then(console.log("----------------------"))
-    .catch(console.error);
+    // .catch(console.error);
+
 };
 
